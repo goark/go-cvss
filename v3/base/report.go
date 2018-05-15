48d832d5eeb9304953dd240f994b546ddc4aac25
Spiegel <spiegel.im.spiegel@gmail.com>
2018-05-15 13:39:53 +0900

    Add base.Metrics.Report() funtion

---
+++ v3/base/report.go
@@ -0,0 +1,86 @@
+package base
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"text/template"
+
+	"golang.org/x/text/language"
+)
+
+const tempCsv = `{{ .AVName }},{{ .AVValue }}
+{{ .ACName }},{{ .ACValue }}
+{{ .PRName }},{{ .PRValue }}
+{{ .UIName }},{{ .UIValue }}
+{{ .SName }},{{ .SValue }}
+{{ .CName }},{{ .CValue }}
+{{ .IName }},{{ .IValue }}
+{{ .AName }},{{ .AValue }}
+`
+
+type CVSSReport struct {
+	AVName, AVValue string //AttackVector
+	ACName, ACValue string //AttackComplexity
+	PRName, PRValue string //PrivilegesRequired
+	UIName, UIValue string //UserInteraction
+	SName, SValue   string //Scope
+	CName, CValue   string //ConfidentialityImpact
+	IName, IValue   string //IntegrityImpact
+	AName, AValue   string //AvailabilityImpact
+}
+
+func (m *Metrics) Report(r io.Reader, lang language.Tag) (io.Reader, error) {
+	buf := &bytes.Buffer{}
+	if t, err := template.New("Repost").Parse(getTemplate(r)); err != nil {
+		return buf, err
+	} else if err = t.Execute(buf, m.getReport(lang)); err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func getTemplate(r io.Reader) string {
+	if r != nil {
+		tmpdata := &strings.Builder{}
+		io.Copy(tmpdata, r)
+		return tmpdata.String()
+	}
+	return tempCsv
+}
+
+func (m *Metrics) getReport(lang language.Tag) *CVSSReport {
+	return &CVSSReport{
+		AVName:  m.AV.Name(lang),
+		AVValue: m.AV.NameOfValue(lang),
+		ACName:  m.AC.Name(lang),
+		ACValue: m.AC.NameOfValue(lang),
+		PRName:  m.PR.Name(lang),
+		PRValue: m.PR.NameOfValue(lang),
+		UIName:  m.UI.Name(lang),
+		UIValue: m.UI.NameOfValue(lang),
+		SName:   m.S.Name(lang),
+		SValue:  m.S.NameOfValue(lang),
+		CName:   m.C.Name(lang),
+		CValue:  m.C.NameOfValue(lang),
+		IName:   m.I.Name(lang),
+		IValue:  m.I.NameOfValue(lang),
+		AName:   m.A.Name(lang),
+		AValue:  m.A.NameOfValue(lang),
+	}
+}
+
+/* Copyright 2018 Spiegel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
