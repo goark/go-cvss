4fc8086796fdae1683a7980808d927967c39a36c
Spiegel <spiegel.im.spiegel@gmail.com>
2023-01-31 15:14:45 +0900

    Migrated v2/base to v2/metric package

--- v2/cvss.go
+++ v2/cvss.go
@@ -1,20 +1,22 @@
 package v2
 
-import "github.com/goark/go-cvss/v2/base"
+import (
+	"github.com/goark/go-cvss/v2/metric"
+)
 
 // CVSS is type of CVSS
 type CVSS struct {
-	Base *base.Metrics
+	Base *metric.Base
 }
 
 // New returns CVSS instance
 func New() *CVSS {
-	return &CVSS{Base: base.NewMetrics()}
+	return &CVSS{metric.NewBase()}
 }
 
 // ImportBaseVector imports CVSSv2.0 base metrics vector
 func (c *CVSS) ImportBaseVector(v string) error {
-	m, err := base.Decode(v)
+	m, err := c.Base.Decode(v)
 	if err != nil {
 		return err
 	}
@@ -23,3 +25,4 @@ func (c *CVSS) ImportBaseVector(v string) error {
 }
 
 /* Copyright 2022 luxifer */
+/* Contributed by Spiegel, 2023 */
